ddb8e789ff8bc9ff3650930d5e6efd6a9e4f0c48
Bhavishya Mohan Aggarwal <130919943+BHAVISHYA2005@users.noreply.github.com>
2026-03-31 10:39:10 +0530

    Create Foundation.py with basic input and calculations
    Added a Python script with multiple input operations and calculations.

---
+++ Foundation.py
@@ -0,0 +1,28 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+# sum = a + b 
+# print("The sum of a+b is:", sum)
+
+
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+# c = int(input("Enter the value of c: "))
+# product = a * b * c
+# print(product)
+# print("THE PRODUCT OF THESE 3 IS: ", product)
+
+
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+# c = (a + b)/2
+# print(c)
+# print("THE AVERAGE: ",c)
+# a = int(input("Enter the value of a: "))
+# if(a > 15):
+#     print("Number is needed")
+# elif(a>10 and a<12 ):
+#     print("Number not needed")
+# else:
+#     print("Haha confusing")
